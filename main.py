fac42cd0ec6c8f18982ce3957ce2365607c5b494
pooopkaaa <60960322+pooopkaaa@users.noreply.github.com>
2021-03-26 23:21:43 +0300

    Get the number of active passcards

--- main.py
+++ main.py
@@ -11,9 +11,11 @@ from datacenter.models import Passcard, Visit
 if __name__ == "__main__":
     # Программируем здесь
     passcards = Passcard.objects.all()
-    
-    for passcard in passcards[:1]:
-        print(f'owner_name: {passcard.owner_name}')
-        print(f'passcode: {passcard.passcode}')
-        print(f'created_at: {passcard.created_at}')
-        print(f'is_active: {passcard.is_active}')
+    active_passcards = []
+
+    for passcard in passcards:
+        if passcard.is_active:
+            active_passcards.append(passcard)
+
+    print('Всего пропусков:', Passcard.objects.count())
+    print('Активных пропусков', len(active_passcards))
