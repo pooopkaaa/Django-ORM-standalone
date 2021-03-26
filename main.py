a795972ea66c283932ab8c145e1794cfb38cc92a
pooopkaaa <60960322+pooopkaaa@users.noreply.github.com>
2021-03-26 23:04:46 +0300

    Get info from first passcard

--- main.py
+++ main.py
@@ -11,5 +11,9 @@ from datacenter.models import Passcard, Visit
 if __name__ == "__main__":
     # Программируем здесь
     passcards = Passcard.objects.all()
-    print(passcards)
-    print('Количество пропусков:', Passcard.objects.count())
+    
+    for passcard in passcards[:1]:
+        print(f'owner_name: {passcard.owner_name}')
+        print(f'passcode: {passcard.passcode}')
+        print(f'created_at: {passcard.created_at}')
+        print(f'is_active: {passcard.is_active}')
