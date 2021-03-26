0e40c14b44063789cf95c0a044715d859aaaf228
pooopkaaa <60960322+pooopkaaa@users.noreply.github.com>
2021-03-26 22:52:21 +0300

    Get passcards from db

--- main.py
+++ main.py
@@ -10,4 +10,6 @@ from datacenter.models import Passcard, Visit
 
 if __name__ == "__main__":
     # Программируем здесь
+    passcards = Passcard.objects.all()
+    print(passcards)
     print('Количество пропусков:', Passcard.objects.count())
